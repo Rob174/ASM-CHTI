37ba4f324f272e2f8538f5d7dfc1f54a66cec8cf
EyeXion <52245783+EyeXion@users.noreply.github.com>
2020-03-25 16:59:19 +0100

    Changements sur le module

--- calcul_dft/CalculModule.s
+++ calcul_dft/CalculModule.s
@@ -5,10 +5,13 @@
 calculModule	proc
 	;r0 : Im
     ;r1 : Re
+	push {lr}
 	mov r3, #0
     smlal r0,r3,r0,r0
-    smlal r2,r0,r1,r1
-    and r2, #0xFFFFFFFF00000000
+    smlal r3,r0,r1,r1
+	str r3, [r2]
+    ;and r2, #0xFFFFFFFF
+	pop {pc}
 	endp
 	end
 	
