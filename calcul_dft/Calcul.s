d5ef87b7e8b6a06d85370214ea28edb68c9d5b1a
EyeXion <52245783+EyeXion@users.noreply.github.com>
2020-03-25 15:51:07 +0100

    Calcul Im ok

--- calcul_dft/Calcul.s
+++ calcul_dft/Calcul.s
@@ -1,25 +1,26 @@
 
 	thumb
 	area moncode, code, readwrite
-	export calculSin
+	export calcul
 	import TabCos
 	import TabSin
-	import LeSignal
-calcul proc
+	import TabSig
+calculIm proc
 	push {lr}
+	push {r0}
 	; r0 contiendra k
-	push {r1}
-	mov r1, r0
 	bl	calculSin
-	pop {r1}
+	pop {r0}
+	str	r12, [r0]
 	pop {pc}
 	endp
+
 calculSin proc 
 	push {r4}
 	push {r5}
 	push {r6}
     ; r0 @ signal origine
-	ldr r0, =LeSignal
+	ldr r0, =TabSig
     ; r1 valeur de k
     ; r2 adresse de la table
 	ldr r2, =TabSin
@@ -35,18 +36,17 @@ debutBcl mul r6, r1, r3
 	; on se décale jusqu'à l'indice désiré (arg angle)
     ldrsh r4, [r2, r6, LSL #1] ; r4 contient le sin(ik2pi/N)
 	; on multiplie par l'échantillon
-    ldrsh r5, [r0, , LSL #1]; r5 contient x(i)
+    ldrsh r5, [r0, r6, LSL #1]; r5 contient x(i)
     mla r12, r5, r4, r12 ; on multiplie et on ajoute le resultat aux precedants
 	; on incremente le pas et on retourne au debut si i est inferieur à N
 	add r3, #1
-	sub r5, r3, #64
-	cbnz r5, debutBcl
-    sub #0, r12 ; on ajoute le -
+	cmp r3, #64
+	bne debutBcl
+   ; sub #0, r12 ; on ajoute le -
 	; la valeur de retour est en r12
 	pop {r6}
 	pop {r5}
 	pop {r4}
 	bx	lr
-	
-    endp
+	endp
     end
--- calcul_dft/main.c
+++ calcul_dft/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void calcul(int * resIm, int * resRe,int * module, int k);
+int * resRe;
+int * resIm;
+int * module;
+
 int main(){
-		
+		res = malloc(sizeof(int));
+		calcul(res, 1);
 		while(1){}
 }
